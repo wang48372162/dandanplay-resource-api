5bba140a53fd7169965282fcd4f11e478bd595ac
wang48372162 <wang48372162@gmail.com>
2023-05-11 19:03:58 +0800

    Tweak `setProxy()`

--- src/provider.ts
+++ src/provider.ts
@@ -15,15 +15,13 @@ export default abstract class Provider {
   }
 
   setProxy(proxyConfig?: AxiosProxyConfig) {
-    const proxy = Object.assign(
-      {}, this.axiosConfig?.proxy, proxyConfig
-    )
+    if (typeof this.axiosConfig !== 'object') {
+      this.axiosConfig = {}
+    }
+
+    const proxy = Object.assign({}, this.axiosConfig.proxy, proxyConfig)
 
     if (Object.keys(proxy).length) {
-      if (typeof this.axiosConfig !== 'object') {
-        this.axiosConfig = {}
-      }
-
       this.axiosConfig.proxy = proxy
     }
 
